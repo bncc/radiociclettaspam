c49ccfb3b394b12abc183fccf643db01a2ad520a
bncc <pucchos@gmail.com>
2013-02-14 08:19:28 -0800

    minor fixes, dependencies file and facebook_spammer in a not-working copy

--- Spammer/src/facebook_spammer.py
+++ Spammer/src/facebook_spammer.py
@@ -1,6 +1,12 @@
 import sys
+
+import pycore
+
 import logger
+
 import conf_handler
+
+import facebook
 
 def publish( status ):
 
@@ -10,9 +16,26 @@ def publish( status ):
 
         conf_path = "facebook.conf"
 
-#	consumer_key = conf_handler.read_conf("consumer_key", conf_path )
-#	if ( consumer_key == None ): 
-#		logger.log("Unable to retrieve consumer_key", -1)
-#		return None
+        api_key = conf_handler.read_conf("api_key", conf_path )
+        if ( api_key == None ): 
+		logger.log("Unable to retrieve consumer_key", -1)
+		return None
+        
+        secret_key = conf_handler.read_conf("secret_key", conf_path )
+        if ( api_key == None ): 
+		logger.log("Unable to retrieve consumer_key", -1)
+		return None
 
-	# Errors must be handled here and then
+        fb = facebook.Facebook(api_key, secret_key)
+
+        token = fb.auth.createToken()
+        
+        logger.log("Token created successfully: " + str(token), 0 )
+
+        # fb.login()
+        # fb.
+        fb.auth.getSession()
+
+        fb.status.set("Ascoltate tanta Radiocicletta!", fb.uid)
+        
+        
--- Spammer/src/pycore.py
+++ Spammer/src/pycore.py
@@ -1,2 +1,3 @@
 import sys
 sys.path.insert(0, "../../pycore");
+sys.path.insert(0, "../lib");
